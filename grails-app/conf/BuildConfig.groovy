8f5c4a7e1144e7fb167132339de2b76080beb849
Andy Lewis <andy.lewis.jr@gmail.com>
2019-02-23 11:21:02 -0800

    Replacing embedded MySql library with Maven dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.16'
+        compile 'mysql:mysql-connector-java:5.1.10'
         compile "cglib:cglib:2.2.2"
 
     }
